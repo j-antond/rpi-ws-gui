482f1053080e2e2b2a074aed3bdd7aacc91f5397
j-antond <javieranton.dev@gmail.com>
2025-07-17 08:41:35 +0200

    Created test folder and test.py file

---
+++ test/test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from src.main import main
+
+class TestMain(unittest.TestCase):
+    def test_main_runs(self):
+        # Solo verificamos que se ejecuta sin errores
+        try:
+            main()
+        except Exception as e:
+            self.fail(f"main() lanzó una excepción: {e}")
+
+if __name__ == "__main__":
+    unittest.main()
