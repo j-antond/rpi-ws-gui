14b040678a135ba13c734e70732599e25735e93f
j-antond <javieranton.dev@gmail.com>
2025-07-18 09:43:13 +0200

    Updated test folder and add test_main.py (check HMI errors)

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,11 @@
 import unittest
-from src.main import main
+from src.main import main2
 
 class TestMain(unittest.TestCase):
     def test_main_runs(self):
         # Solo verificamos que se ejecuta sin errores
         try:
-            main()
+            main2()
         except Exception as e:
             self.fail(f"main() lanzó una excepción: {e}")
 
